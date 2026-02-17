48c1ef7f87eb35332f14af285833baa241994fe6
MATHIAZHAGAN MS <146163288+Mathi-MS@users.noreply.github.com>
2026-02-17 20:39:14 +0530

    Update mobile number and address in CommonWord

--- src/Custom/CommonWord.tsx
+++ src/Custom/CommonWord.tsx
@@ -1,6 +1,6 @@
 export const commonWord = {
   title:"MercuryLoans",
-  mobileNumber:"9999999999",
+  mobileNumber:"9150781545",
   email:"mercuryloans00@gmail.com",
-  address:"CR.NO:789, Gandhi Nagar, Coimbatore, Tamil Nadu 641045",
+  address:"No. 89 Ansari Street, Ram Nagar, Coimbatore - 641009.",
 };
